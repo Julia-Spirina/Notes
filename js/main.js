2e3aad9071ebb018c6887f1e0b4b2c1250b2fbdd
Julia-Spirina <julieraskoshnaya@gmail.com>
2023-02-06 09:13:02 +0700

    notes,note-add

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
-Vue.component('product', {
+Vue.component('notes', {
     template: `
-<div>
-   <div class="column">
-   </div>
-   <div class="column">
-   </div>
-   <div class="column">
+<div class="notes">
+<note-add></note-add>
+   <div class="all-cards">
+       <div class="note">
+       <h2>one</h2>
+       </div>
+       <div class="note">
+       <h2>two</h2>
+       </div>
+       <div class="note">
+       <h2>three</h2>
+       </div>
    </div>
   </div>
  `,
     data() {
         return {
-            // тут будут данные
+            noteOne:[],
+            noteTwo:[],
+            noteTree:[],
         }
     },
+    methods: {
+        // тут будут методы
+    },
+    computed: {
+        // тут будут вычисляемые свойства
+    }
+})
+
+
+Vue.component('note-add', {
+    template: `
+        <div>
+            <form class="note-form">
+            <label for="name" class="label-form">Name</label>
+            <input class="input-form" id="task" v-model="name" maxlength="50" required>
+<!--            <div class="name-column">-->
+<!--                <label for="name" class="label-form">Name</label>-->
+<!--                <input class="input-form" id="task1" v-model="task1" maxlength="50" required>-->
+<!--            </div>-->
+         
+</form>
+        </div>
+    `
+    ,
     methods: {
         // тут будут методы
     },
